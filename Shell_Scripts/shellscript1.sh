587cf1d0d045797beb0c0d6ccfe02f76186bb06f
soniyastellamc <soniyastellamc@gmail.com>
2018-02-20 08:12:14 +0530

    Shell Scripting 1 and 2

---
+++ Shell_Scripts/shellscript1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#1
+echo "Hello World"
+x=$(logname)
+echo "Currently logged in as : $x"
+echo "Current Shell : $SHELL"
+echo "Home Directory : $HOME"
+x=$(arch)
+echo "Architecture : $x"
+echo "Current Path : $PATH"
+echo "pwd : $pwd"
+echo "Logged in users : $users"
+
---
+++ Shell_Scripts/shellscript2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#2
+clear
+x=$(lsb_release -a)
+echo "OS Details : $x"
+echo "Available Shells :" 
+echo $(cat /etc/shells)
+echo "Mouse settings : "
+echo $(xinput --list --short)
+echo "CPU Information : "
+echo $(lscpu)
+echo "Memory  Information :"
+echo $(free -m)
+echo "File system :"
+echo $(sudo fdisk -l)
+
